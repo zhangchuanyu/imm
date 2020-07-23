9b176411591b4c5a70bb8d1b6e88f2f199dd899e
albab <albab.saifi@byo.social>
2020-07-23 09:04:33 -0400

    Home page first revision
    All sections done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	$(".courses-carousel").owlCarousel({
+  	items:1,
+  	loop: true,
+  	autoplay: true,
+    margin:0,
+    stagePadding:0,
+    nav : true
+  });
   $(".owl-carousel").owlCarousel({
   	items:1,
   	loop: true,
@@ -6,4 +14,7 @@ $(document).ready(function(){
     stagePadding:0,
     nav : true
   });
+
+
+
 });
